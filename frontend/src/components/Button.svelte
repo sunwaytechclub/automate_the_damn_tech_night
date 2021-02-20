f740f9cccf5a35e5b08bb950dc8163ea99d913e7
frankeyey <frankeyc777@gmail.com>
2021-02-20 21:07:47 +0800

    add loading and disabled behaviour

--- frontend/src/components/Button.svelte
+++ frontend/src/components/Button.svelte
@@ -1,9 +1,21 @@
 <script>
+    import { Circle } from 'svelte-loading-spinners'
+
     export let label="Click Me"
     export let secondaryButton = false;
+    export let loading;
+    export let disabled;
 </script>
 
-<button on:click {...$$restProps} class="{secondaryButton ? "secondary-button" : ""}">{label}</button>
+<button on:click {...$$restProps} class="{secondaryButton ? "secondary-button" : ""} {disabled || loading ? "disabled" : ""}" disabled={disabled || loading}>
+    {#if loading}
+    <span class="loader-span">
+        <Circle color="#E7E4FA" size="30"/>
+    </span>
+    {:else}
+        {label}
+    {/if}
+</button>
 
 <style>
     button {
@@ -22,5 +34,13 @@
         background-color: var(--white);
         color: var(--purple-2);
         border: 2px var(--purple-2) solid;
+    }
+    .loader-span {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+    }
+    .disabled {
+        cursor: auto;
     }
 </style>
