6d8a8ad05bc0f0c74a8aa2db0bdad54014cc5c8a
frankeyey <frankeyc777@gmail.com>
2021-02-21 00:06:38 +0800

    add label binding

--- frontend/src/components/Checkbox.svelte
+++ frontend/src/components/Checkbox.svelte
@@ -1,5 +1,6 @@
 <script>
     export let checked = false;
+    export let label = "Checkbox"
     export let subText = ""
 
     function handleCheck() {
@@ -9,7 +10,7 @@
 <div class="wrapper">
     <div class="checkbox-div">
         <img src="/assets/icons/circle-{checked ? "check-yellow" : "yellow"}.svg" alt="unchecked icon" on:click={handleCheck}/>
-        <p class="checkbox-text">Post on Facebook Group</p>
+        <p class="checkbox-text">{label}</p>
     </div>
     {#if subText}
         <p class="checkbox-subtext">{subText}</p>
@@ -30,11 +31,13 @@
         display: inline;
         color: var(--yellow);
         font: var(--primary-font-semibold);
+        font-size: 14px;
         margin-left: 12px;
     }
     .checkbox-subtext {
         color: var(--purple-1);
-        font-size: 12px;
+        font: var(--primary-font-regular);
+        font-size: 10px;
         margin-left: 36px;
     }
 </style>
