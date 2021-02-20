aa4d5ac3b528fea5bf8ad33482ca319963c7afed
frankeyey <frankeyc777@gmail.com>
2021-02-21 00:08:01 +0800

    add label and error binding

--- frontend/src/components/DatePicker.svelte
+++ frontend/src/components/DatePicker.svelte
@@ -5,24 +5,43 @@
     import 'flatpickr/dist/themes/light.css'
 
     export let date = null
+    export let label = "Date"
+    export let error = {
+        enabled: false,
+        message: "Invalid"
+    }
+    
     const flatpickrOptions = {
         element: '#my-date-picker'
     }
     
 </script>
+<div>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label>{label}</label>
 
-<Flatpickr options="{ flatpickrOptions }" bind:value={date} element="#my-date-picker">
-    <div class="flatpickr" id="my-date-picker">
-        <input type="text" placeholder="Select Date.." data-input>
-
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a class="input-button" title="clear" data-clear>
+    <Flatpickr options="{ flatpickrOptions }" bind:value={date} element="#my-date-picker">
+        <div class="flatpickr" id="my-date-picker">
+            <input type="text" placeholder="Select Date.." data-input class="{error.enabled ? "error" : ""}">
+            {#if error.enabled}
+                <p class="error-message">{error.message}</p>
+            {/if}
+            
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a class="input-button" title="clear" data-clear>
                 <i class="icon-close"></i>
-        </a>
-    </div>
-</Flatpickr>
+            </a>
+        </div>
+    </Flatpickr>
+</div>
 
 <style>
+    label {
+        font-weight: 600;
+        margin-bottom: 10px;
+        display: block;
+        color: var(--dark-blue);
+    }
     input {
         height: 51px;
         width: 100%;
@@ -31,6 +50,28 @@
         background-color: var(--light-grey);
         border: none;
         padding: 0 20px;
+    }
+    .error {
+        border: 2px var(--red) solid;
+    }
+    .error-message {
+        font: var(--primary-font-regular);
+        font-size: 12px;
+        color: var(--red);
+        margin-bottom: 15px;
+        margin-top: 5px;
+    }
+    ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: var(--dark-grey);
+        opacity: 1; /* Firefox */
+    }
+
+        :-ms-input-placeholder { /* Internet Explorer 10-11 */
+        color: var(--dark-grey);
+    }
+
+        ::-ms-input-placeholder { /* Microsoft Edge */
+        color: var(--dark-grey);
     }
     .flatpickr {
         margin-bottom: 15px;
--- frontend/src/components/TimePicker.svelte
+++ frontend/src/components/TimePicker.svelte
@@ -5,6 +5,12 @@
     import 'flatpickr/dist/themes/light.css'
 
     export let time = null
+    export let label = "Time"
+    export let error = {
+        enabled: false,
+        message: "Invalid"
+    }
+    
     const flatpickrOptions = {
         element: '#my-time-picker',
         enableTime: true,
@@ -14,18 +20,41 @@
     
 </script>
 
-<Flatpickr options="{ flatpickrOptions }" bind:value={time} element="#my-time-picker">
-    <div class="flatpickr" id="my-time-picker">
-        <input type="text" placeholder="Select Time.." data-input>
-        
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a class="input-button" title="clear" data-clear>
-                <i class="icon-close"></i>
-        </a>
-    </div>
-</Flatpickr>
+<div>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label>{label}</label>
+
+    <Flatpickr options="{ flatpickrOptions }" bind:value={time} element="#my-time-picker">
+        <div class="flatpickr" id="my-time-picker">
+            <input type="text" placeholder="Select Time.." data-input class="{error.enabled ? "error" : ""}">
+            {#if error.enabled}
+                <p class="error-message">{error.message}</p>
+            {/if}
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a class="input-button" title="clear" data-clear>
+                    <i class="icon-close"></i>
+            </a>
+        </div>
+    </Flatpickr>
+</div>
 
 <style>
+    label {
+        font-weight: 600;
+        margin-bottom: 10px;
+        display: block;
+        color: var(--dark-blue);
+    }
+    .error {
+        border: 2px var(--red) solid;
+    }
+    .error-message {
+        font: var(--primary-font-regular);
+        font-size: 12px;
+        color: var(--red);
+        margin-bottom: 15px;
+        margin-top: 5px;
+    }
     input {
         height: 51px;
         width: 100%;
@@ -37,5 +66,17 @@
     }
     .flatpickr {
         margin-bottom: 15px;
+    }
+    ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+        color: var(--dark-grey);
+        opacity: 1; /* Firefox */
+    }
+
+        :-ms-input-placeholder { /* Internet Explorer 10-11 */
+        color: var(--dark-grey);
+    }
+
+        ::-ms-input-placeholder { /* Microsoft Edge */
+        color: var(--dark-grey);
     }
 </style>
