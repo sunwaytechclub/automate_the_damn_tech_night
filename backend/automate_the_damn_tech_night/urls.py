1a24cca11fd410713c189e69eb0628304a9c04da
pupubird <rainchai4240@gmail.com>
2021-02-23 22:55:14 +0800

    Create email and speaker app

--- backend/automate_the_damn_tech_night/urls.py
+++ backend/automate_the_damn_tech_night/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("rest_framework.urls")),
+    path("api/v1/", include("api.urls)),
 ]
