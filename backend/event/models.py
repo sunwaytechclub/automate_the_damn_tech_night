158fd54e3eaed2c4df343d5742024e173f1b3312
pupubird <rainchai4240@gmail.com>
2021-02-26 17:19:30 +0800

    Remove image field in models

--- backend/event/models.py
+++ backend/event/models.py
@@ -5,10 +5,8 @@ from speaker.models import Speaker
 class Event(models.Model):
     episode = models.IntegerField()
     datetime = models.DateTimeField()
-    poster = models.ImageField(upload_to="event_poster")
 
 
 class EventSpeaker(models.Model):
     event = models.ForeignKey(to=Event, on_delete=models.CASCADE)
     speaker = models.ForeignKey(to=Speaker, on_delete=models.CASCADE)
-    poster = models.ImageField(upload_to="speaker_poster")
