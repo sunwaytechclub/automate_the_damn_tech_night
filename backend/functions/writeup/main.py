2e9c1de411e19b723f81644988441d1e8724f86a
pupubird <rainchai4240@gmail.com>
2021-02-27 19:29:24 +0800

    Add writeup in event API response

---
+++ backend/functions/writeup/main.py
@@ -0,0 +1,48 @@
+from event.models import Writeup, Speaker, Event
+import re
+
+
+def main(event):
+    try:
+        from event.serializers import EventCreateSerializer
+    except ImportError:
+        pass
+    event_serializer = EventCreateSerializer(instance=event).data
+
+    writeup = Writeup.objects.first()
+    content = writeup.content
+    res = re.finditer("{{ .{1,10} }}", content)
+    switch = {
+        "hooks": hooks,
+        "topics": topics,
+        "episode": episode,
+        "datetime": datetime,
+    }
+    for result in res:
+        # span = [result.start, result.end]
+        param = result.group().strip("{{ ").strip(" }}")
+        value = switch[param](event_serializer, event)
+        content = content.replace(result.group(), str(value))
+    return content
+
+
+def hooks(data, event):
+    hook = ""
+    for topic in data["topic"]:
+        hook += f'"{topic["hook"]}"' + "\n"
+    return hook
+
+
+def topics(data, event):
+    topic = ""
+    for t in data["topic"]:
+        topic += t["title"] + "!" + "\n"
+    return topic
+
+
+def episode(data, event):
+    return event.episode
+
+
+def datetime(data, event):
+    return event.datetime.strftime("%d.%m.%Y %I:%M %p")
