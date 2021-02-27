5730e53a22412448fb3414c297e5c6696b83e5df
frankeyey <frankeyc777@gmail.com>
2021-02-27 16:10:45 +0800

    add Card component

---
+++ frontend/src/components/Card.svelte
@@ -0,0 +1,12 @@
+<div class="wrapper">
+    <slot></slot>
+</div>
+
+<style>
+   .wrapper {
+        background: #FFFFFF;
+        box-shadow: 2px 7px 24px rgba(0, 0, 0, 0.09);
+        border-radius: 5px;
+        width: 100%;
+    }
+</style>
