9c99b463cc37141b024dc1e5ed4e18cea1f83c05
pupubird <rainchai4240@gmail.com>
2021-02-27 14:29:38 +0800

    Add secret key with dotenv

--- backend/manage.py
+++ backend/manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automate_the_damn_tech_night.settings')
+    load_dotenv()
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "automate_the_damn_tech_night.settings"
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
