f3e85410136d1b4f3309bc2f9cb7df72764db67d
frankeyey <frankeyc777@gmail.com>
2021-02-28 14:14:47 +0800

    update SideNavbar

--- frontend/src/components/SideNavbar.svelte
+++ frontend/src/components/SideNavbar.svelte
@@ -1,4 +1,7 @@
 <script>
+    import AlertDialog from "@/components/AlertDialog.svelte"
+    import Button from "@/components/Button.svelte";
+
     let pathName = location.pathname
     export let navs = [
         {
@@ -17,18 +20,42 @@
             navPath: "/logout"
         }
     ]
+
+    let logoutDialog = false
+
+    function logout() {
+        logoutDialog = true;
+    }
 </script>
 
 <div class="wrapper">
     <img src="/assets/stc-logo.svg" alt="stc logo" class="stc-icon"/>
 
     {#each navs as nav}
-        <div class="navigation {pathName.includes(nav.navPath) ? "active" : ""}">
-            <img src={nav.iconPath} alt="" class="nav-icon"/>
-            <p class="nav-title">{nav.navTitle}</p>
-        </div>
+        {#if nav.navTitle == "Logout"}
+            <div class="navigation {pathName.includes(nav.navPath) ? "active" : ""}" on:click={logout}>
+                <img src={nav.iconPath} alt="" class="nav-icon"/>
+                <p class="nav-title">{nav.navTitle}</p>
+            </div>
+        {:else}
+            <a href={nav.navPath} class="navigation {pathName.includes(nav.navPath) ? "active" : ""}">
+                <img src={nav.iconPath} alt="" class="nav-icon"/>
+                <p class="nav-title">{nav.navTitle}</p>
+            </a>
+        {/if}
     {/each}
 </div>
+
+{#if logoutDialog}
+    <AlertDialog bind:visible={logoutDialog}>
+        <p class="logout-title">Are you sure?</p>
+        <p class="logout-message">Are you sure want to logout?</p>
+        <div class="logout-button-div">
+            <Button secondaryButton on:click={() => logoutDialog=false} label="Cancel"/>
+            <Button on:click label="Yes"/>
+        </div>
+    </AlertDialog>
+{/if}
 
 <style>
     .wrapper {
@@ -50,6 +77,7 @@
         margin-bottom: 10px;
         cursor: pointer;
         width: 170px;
+        text-decoration: none;
     }
     .active {
         background-color: var(--light-purple-2);
@@ -61,5 +89,19 @@
         font: var(--primary-font-bold);
         font-size: 12px;
         color: var(--purple-1);
+    }
+    .logout-title {
+        font: var(--primary-font-bold);
+        font-size: 24px;
+        margin-bottom: 20px;
+    }
+    .logout-message {
+        font: var(--primary-font-semibold);
+        margin-bottom: 40px;
+    }
+    .logout-button-div {
+        display: grid;
+        grid-template-columns: auto auto;
+        column-gap: 30px;
     }
 </style>
