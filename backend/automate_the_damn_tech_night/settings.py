f29c248115aa64c77ebce18c07f565697498b833
pupubird <rainchai4240@gmail.com>
2021-03-02 02:33:51 +0800

    Update config

--- backend/automate_the_damn_tech_night/settings.py
+++ backend/automate_the_damn_tech_night/settings.py
@@ -134,6 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "media/"
