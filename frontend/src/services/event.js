a008873cacc6732683412757791bd463c7fd30c2
pupubird <rainchai4240@gmail.com>
2021-03-02 23:16:15 +0800

    Update event API to remove null

--- frontend/src/services/event.js
+++ frontend/src/services/event.js
@@ -44,14 +44,11 @@ export default {
 	 * @returns {Event} n
 	 */
 	async updateEvent({ id, episode = null, datetime = null, topic = null }) {
+		let body = { episode, datetime, topic };
+		Object.keys(body).map((key) => !body[key] && delete body[key]);
 		let event = await Network.authPatch({
 			path: `/api/v1/event/${id}/`,
-			body: {
-				id,
-				episode,
-				datetime,
-				topic,
-			},
+			body,
 		});
 		return event;
 	},
